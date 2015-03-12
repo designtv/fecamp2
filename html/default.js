05d15f32164b34f5a3997179094cef724a01b672
designtv <hilex@naver.com>
2015-03-12 20:07:06 +0900

    js

--- html/default.js
+++ html/default.js
@@ -1,5 +1,42 @@
 jQuery(function($){
-	$(".gnb>button").on("click", function(){
-		$(".gnb>ul").slideToggle(200);
+	// 모바일 GNB 토글 버튼
+	var $gnbBtn = $(".gnb>button");
+	
+	$gnbBtn.on("click", function(){
+		$(this).next().slideToggle(200);
+
+		if( $gnbBtn.text() === "메뉴 열기" ) {
+			//메뉴 닫기 텍스트 표시
+			$gnbBtn.text("메뉴 닫기");
+		} else{
+			//메뉴 열기 텍스트 표시
+			$gnbBtn.text("메뉴 열기");
+		}
+	});
+
+	// 태블릿 + 데스크톱 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover fucus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
